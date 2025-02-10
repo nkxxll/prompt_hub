8dd623ec68a882470a3fc7bfab5d66a7c60960be
nkxxll <niki.nice1203@gmail.com>
2025-02-10 16:18:49 +0100

    chore: flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Basic flake for Bun.js project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+    outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+
+          shellHook = ''
+            echo "Bun.js development environment"
+          '';
+        };
+      });
+}
