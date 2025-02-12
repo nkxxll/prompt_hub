479f7336bc23306c5f9a6a41214c5584c57bd042
nkxxll <niki.nice1203@gmail.com>
2025-02-12 23:52:43 +0100

    feat: ag grid with prompts

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { type GridApi, createGrid, type GridOptions, ModuleRegistry } from "ag-grid-community";
+  import { ClientSideRowModelModule } from "ag-grid-community";
+  import { themeQuartz } from "ag-grid-community";
+
+  ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+  let { data, columnDefs } = $props();
+
+  let gridElement: HTMLDivElement;
+  let gridApi: GridApi;
+
+  const myTheme = themeQuartz.withParams({
+    accentColor: "#15BDE8",
+    backgroundColor: "#0C0C0D",
+    borderColor: "#ffffff00",
+    borderRadius: 20,
+    browserColorScheme: "dark",
+    cellHorizontalPaddingScale: 1,
+    chromeBackgroundColor: {
+      ref: "backgroundColor",
+    },
+    columnBorder: false,
+    fontFamily: {
+      googleFont: "Roboto",
+    },
+    fontSize: 16,
+    foregroundColor: "#BBBEC9",
+    headerBackgroundColor: "#182226",
+    headerFontSize: 14,
+    headerFontWeight: 500,
+    headerTextColor: "#FFFFFF",
+    headerVerticalPaddingScale: 0.9,
+    iconSize: 20,
+    rowBorder: false,
+    rowVerticalPaddingScale: 1.2,
+    sidePanelBorder: false,
+    spacing: 8,
+    wrapperBorder: true,
+    wrapperBorderRadius: 10,
+  });
+
+  const gridOptions: GridOptions = {
+    columnDefs,
+    rowData: data,
+    rowSelection: "single",
+    defaultColDef: {
+      resizable: true,
+    },
+    rowModelType: "clientSide",
+    theme: myTheme,
+  };
+
+  onMount(() => {
+    gridApi = createGrid(gridElement, gridOptions);
+  });
+</script>
+
+<div bind:this={gridElement} class="w-full rounded-xl ag-theme-alpine" style="height: 80vh"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
-	import type { PageProps } from './$types';
+  import type { PageProps } from "./$types";
+  import Table from "$lib/components/Table.svelte";
 
-	let { data }: PageProps = $props();
+  let { data }: PageProps = $props();
+  const columnDefs = [
+    { field: "id", sortable: true, filter: true },
+    { field: "name", sortable: true, filter: true },
+    { field: "formatString", sortable: true, filter: true },
+    { field: "notes", sortable: true, filter: true },
+    { field: "llmWebsite", sortable: true, filter: true },
+    { field: "category", sortable: true, filter: true },
+    { field: "usageCount", sortable: true, filter: true },
+    { field: "public", sortable: true, filter: true },
+    { field: "userId", sortable: true, filter: true },
+    { field: "createAt", sortable: true, filter: true },
+    { field: "updatedAt", sortable: true, filter: true },
+  ];
 </script>
 
-{#each data.patterns as pattern}
-  <p>{pattern.name}</p>
-{/each}
+<Table data={data.patterns} {columnDefs} />
